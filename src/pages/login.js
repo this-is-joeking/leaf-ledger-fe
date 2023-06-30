56bc1647f4f0e48f7b054bdbc21edd8c79630f85
Joe King <j.king92@gmail.com>
2023-06-30 13:33:54 -0700

    Add navbar and initial pages

---
+++ src/pages/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+const Login = () => {
+    return (
+        <div>
+            <h1>Here is where users will be able to signup with google OAuth in the future</h1>
+        </div>
+    );
+};
+ 
+export default Login;
---
+++ src/pages/plants.js
@@ -0,0 +1,9 @@
+import React from 'react';
+ 
+const Plants = () => {
+    return (
+        <h1>Details about plants already on leaf ledger are found here.</h1>
+    );
+};
+ 
+export default Plants;
