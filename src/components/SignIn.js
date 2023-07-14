973a275e9193003d61d3df0e9a12dca362e88723
Joe King <j.king92@gmail.com>
2023-07-14 14:32:24 -0700

    Linting

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,37 +2,34 @@ import React, { useState, useEffect } from 'react';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 
-export default function Plantlist() {
+export default function SignIn() {
   const [ user, setUser ] = useState([]);
 	const [ profile, setProfile ] = useState([]);
 
 	const login = useGoogleLogin({
-			onSuccess: (codeResponse) => setUser(codeResponse),
-			onError: (error) => console.log('Login Failed:', error)
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log('Login Failed:', error)
 	});
 
-	useEffect(
-			() => {
-					if (user) {
-							axios
-									.get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-											headers: {
-													Authorization: `Bearer ${user.access_token}`,
-													Accept: 'application/json'
-											}
-									})
-									.then((res) => {
-											setProfile(res.data);
-									})
-									.catch((err) => console.log(err));
-					}
-			},
-			[ user ]
-	);
+	useEffect(() => {
+    if (user) {
+      axios
+        .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+          headers: {
+            Authorization: `Bearer ${user.access_token}`,
+            Accept: 'application/json'
+          }
+        })
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user] );
 
 	const logOut = () => {
-			googleLogout();
-			setProfile(null);
+    googleLogout();
+    setProfile(null);
 	};
   
 return (
