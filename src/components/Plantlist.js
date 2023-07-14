07c79d5965b419ce5913da94b652be448d4d1880
Joe King <j.king92@gmail.com>
2023-07-14 14:21:56 -0700

    Linting

--- src/components/Plantlist.js
+++ src/components/Plantlist.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Pagination } from '@mui/material';
 
