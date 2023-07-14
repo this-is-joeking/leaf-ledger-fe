f2d70ef2467467948b0f13e79f9841111635200d
Joe King <j.king92@gmail.com>
2023-07-14 09:37:47 -0700

    Add signin component

---
+++ src/components/SignIn.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+
+export default function Plantlist() {
+  const responseMessage = (response) => {
+    console.log(response);
+  };
+
+  const errorMessage = (error) => {
+      console.log(error);
+  };
+  
+return (
+    <div>
+        <h2>React Google Login</h2>
+        <br />
+        <br />
+        <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+    </div>
+  );
+
+}
