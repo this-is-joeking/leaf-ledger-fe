0fd4e7e0e3cf58071f027b85ab98efe0fc9a3930
Joe King <j.king92@gmail.com>
2023-07-14 09:38:12 -0700

    Add signin component to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import SignIn from '../components/SignIn'
  
 const Login = () => {
     return (
         <div>
+            <SignIn />
             <h1>Here is where users will be able to signup with google OAuth in the future</h1>
         </div>
     );
