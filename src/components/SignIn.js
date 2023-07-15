4eafd0f0c0b8b2f124fd87ce198f84cf2806c4c7
Joe King <j.king92@gmail.com>
2023-07-14 23:22:32 -0700

    Most recent iteration of sign in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -8,24 +8,43 @@ export default function SignIn() {
 
 	const login = useGoogleLogin({
     onSuccess: (codeResponse) => setUser(codeResponse),
-    onError: (error) => console.log('Login Failed:', error)
+    onError: (error) => console.log('Login Failed:', error),
+    flow: 'auth-code',
+    access: 'offline'
 	});
 
-	useEffect(() => {
+	// useEffect(() => {
+  //   if (user) {
+  //     axios
+  //       .get(`https://www.googleapis.com/oauth2/v1/userinfo`, {
+  //         headers: {
+  //           Authorization: `Bearer ${user.access_token}`,
+  //           Accept: 'application/json'
+  //         }
+  //       })
+  //       .then((res) => {
+  //         setProfile(res.data);
+  //       })
+  //       .catch((err) => console.log(err));
+  //   }
+  // }, [user] );
+
+  useEffect(() => {
     if (user) {
-      axios
-        .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-          headers: {
-            Authorization: `Bearer ${user.access_token}`,
-            Accept: 'application/json'
-          }
-        })
-        .then((res) => {
-          setProfile(res.data);
-        })
-        .catch((err) => console.log(err));
+      sendUserIdToServer(user);
     }
-  }, [user] );
+  }, [user])
+
+  const sendUserIdToServer = (token) => {
+    axios
+      .post('http://127.0.0.1:3000/api/v1/session', {
+        headers: {
+          GoogleToken: token,
+          Accept: 'application/json'
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
 	const logOut = () => {
     googleLogout();
@@ -37,6 +56,8 @@ return (
       <h2>React Google Login</h2>
           <br />
           <br />
+          {/* {console.log(profile)} */}
+          {console.log(user.getAuthResponse().id_token)}
           {profile ? (
               <div>
                   <img src={profile.picture} alt='user image' />
@@ -45,6 +66,7 @@ return (
                   <p>Email Address: {profile.email}</p>
                   <br />
                   <br />
+                  {/* {console.log(user.access_token)} */}
                   <button onClick={logOut}>Log out</button>
               </div>
           ) : (
